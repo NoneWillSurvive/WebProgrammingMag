c1bb9946b2b552c37f93922dd5374b2e14cd37ca
Богданов Кирилл <bogdanovka@data-platform.ru>
2022-12-22 23:55:43 +0300

    Перенос БД на общий доступ

---
+++ SVP.API/Migrations/20221222204354_MigrationModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SVP.API.Migrations
+{
+    public partial class MigrationModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
